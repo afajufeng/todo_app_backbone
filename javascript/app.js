dd2ba5b21e9b825deae3f28a7b3fd4fb8b0f3462
xiaoling chen <xiaolingchen@xiaolings-MacBook-Air.local>
2016-06-14 17:27:55 +0800

    create dateselector

--- javascript/app.js
+++ javascript/app.js
@@ -2,9 +2,23 @@
 var AppView = Backbone.View.extend({
   el: $("body"),
 
+  initialize: function(){
+    this.buildDate("#day", 1, 30);
+    this.buildDate("#month", 1, 12);
+    this.buildDate("#year", 2000, 2016);
+  },
+
   events: {
     "click #addItem": "addItem",
     "click .modalBack": "hideModal"
+  },
+
+  buildDate: function buildSelect(period, num_start, num_end){
+    var text ="";
+    for(var i = num_start; i <= num_end; i++){
+      text += "<option>" + i + "</option>";
+    }
+    $(period).append(text);
   },
 
   addItem: function(e){
